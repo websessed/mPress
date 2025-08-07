c461b8dd3b42d18a72cd22e010ceddc4e0273d0f
Maxwell Easter <maxwelleaster00@gmail.com>
2025-08-07 19:30:04 +0100

    add project description

---
+++ desc.js
@@ -0,0 +1,34 @@
+
+/* 
+
+core features:
+    -auth via keyphrase (12word mnemonic)
+    -explore feed (filter by new, past)
+    -search (topic, tag, pubkey)
+    -post metadata (views, upvotes, reposts)
+    -user dashboard {
+        pubkey, bio, post count, date joined 
+        publish new, published, drafts 
+    }
+    thread support 
+    settings 
+    
+no:
+    usernames
+    emails 
+    comments 
+    editing/deletion of posts 
+    analytics/tracking
+    censorship 
+    deleted account recovery 
+
+
+built for: 
+    whistleblowers/leakers 
+    citizens under authoritarian regimes 
+    figures in exile/under threat 
+    hacktivists 
+    memory preservationists 
+    people with something to say
+
+*/
